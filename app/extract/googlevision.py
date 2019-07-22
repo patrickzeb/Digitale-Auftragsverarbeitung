ffb00949e4276bd4d6e1475dac83b8a5adb70043
ckuehnel93 <33935032+ckuehnel93@users.noreply.github.com>
2019-07-22 11:03:34 +0200

    Update googlevision.py
    Kleinere Anpassungen an der Funktion zum OCR-Scanning

--- app/extract/googlevision.py
+++ app/extract/googlevision.py
@@ -1,4 +1,3 @@
-import json
 from io import open
 from google.cloud import vision
 import os
@@ -11,15 +10,40 @@ class TextAnnotation(object):
     def __init__(self, text: str):
         self.text = text
 
-        
+
 def _text_detection(image_file: str) -> str:
-    with open(image_file, 'rb') as image:
+    with open('./upload/' + image_file, 'rb') as image:
         content = image.read()
-	
+
     image = vision.types.Image(content=content)
     response = client.document_text_detection(image=image)
-	
-    response = response.text_annotations[0].description
-	
+
+    # response = str(response.text_annotations[0].description).encode("utf-8")
+    response = response.full_text_annotation
+
     return response
 
+
+def google_ocr_alt(files: list) -> (dict):
+    # doc_text = [_text_detection(file) for file in files]
+    doc_text = {}
+    doc_text["Documents"] = {}
+
+    n = 0
+    for file in files:
+        doc_text['Documents'][file] = {'Name': file, 'Beschreibung': "unknown", 'Text': _text_detection(file)}
+        n += 1
+
+    # print(doc_text[1]['text'])
+    return doc_text
+
+
+def ocr(image_file: str) -> str:
+    with open('./upload/' + image_file, 'rb') as image:
+        content = image.read()
+
+    image = vision.types.Image(content=content)
+    response = client.document_text_detection(image=image)
+    response = response.full_text_annotation
+
+    return response
